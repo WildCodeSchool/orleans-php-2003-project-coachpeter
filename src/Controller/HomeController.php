6dec2d557224886d2de3d61eeb5db2a4afb19383
malyadev <malyadev248@gmail.com>
2020-06-02 10:35:18 +0200

    sacré Travis haha

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
 class HomeController extends AbstractController
 {
     /**
