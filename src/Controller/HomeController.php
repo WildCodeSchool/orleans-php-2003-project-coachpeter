fad270bb91716ddfa5794a65349c7c929cb7f4eb
kodev-sudo <k.ollivierdev@gmail.com>
2020-06-05 19:09:40 +0200

    Adding database support for coach infos

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Actuality;
+use App\Entity\InfoCoach;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,11 +15,16 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
+        $coachInfos = $this->getDoctrine()
+            ->getRepository(InfoCoach::class)
+            ->findAll();
+
         $actualities = $this->getDoctrine()
             ->getRepository(Actuality::class)
             ->findBy([], ['date' => 'desc'], 3);
 
         return $this->render('home/home.html.twig', [
+            'coachInfos' => $coachInfos,
             'actualities' => $actualities,
         ]);
     }
