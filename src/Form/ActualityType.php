acdb0c7b8fea73542c4c0cfbfb883b5aa0df3d3c
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-12 09:15:07 +0200

    toujour sur la mise en forme

--- src/Form/ActualityType.php
+++ src/Form/ActualityType.php
@@ -5,11 +5,11 @@ namespace App\Form;
 use App\Entity\Actuality;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Date;
 
 class ActualityType extends AbstractType
 {
@@ -22,14 +22,13 @@ class ActualityType extends AbstractType
                 'label' => 'Article',
             ])
             ->add('date', DateType::class,[
-                'label' => 'Date',
+                'label' => 'Date: jour-mois-année',
                 'format' => 'dd MM yyyy',
             ])
             ->add('theme', TextType::class, [
                 'label' => 'Thème'])
             ->add('picture', TextType::class, [
-                'label' => 'Image'])
-        ;
+                'label' => 'Thème']);
     }
 
     public function configureOptions(OptionsResolver $resolver)
