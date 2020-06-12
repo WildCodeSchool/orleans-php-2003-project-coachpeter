c61a7d0ee9cdfffc714522b76d2a66df85cb9275
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-12 18:34:38 +0200

    fixture semble ok photo ko??

--- src/DataFixtures/TransformFixtures.php
+++ src/DataFixtures/TransformFixtures.php
@@ -15,13 +15,13 @@ class TransformFixtures extends Fixture
         $faker = Faker\Factory::create('fr_FR');
         for ($i = 1; $i <= 4; $i++) {
             $transformation = new Transformation();
-            $transformation->setName($faker->Name(15));
-            $transformation->setPound($faker->numberBetween($min = 5, $max = 20));
-            $transformation->setPictureBefore($faker->imageUrl($width = 640, $height = 480));
-            $transformation->setPictureAfter($faker->imageUrl($width = 640, $height = 480));
+            $transformation->setName($faker->Name('female'));
+            $transformation->setPound($faker->numberBetween(5, 20));
+            $transformation->setPictureBefore($faker->imageUrl(640, 480));
+            $transformation->setPictureAfter($faker->imageUrl());
 
             $manager->persist($transformation);
-          }
+        }
         $manager->flush();
     }
 }
