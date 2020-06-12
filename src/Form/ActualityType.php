0b35c81af74ab7b88267e1b50d667de7d57958a0
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-12 07:29:07 +0200

    debut de la mise en forme avec changement du nom des labels

--- src/Form/ActualityType.php
+++ src/Form/ActualityType.php
@@ -4,19 +4,27 @@ namespace App\Form;
 
 use App\Entity\Actuality;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
 
 class ActualityType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('content')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'])
+            ->add('content', TextareaType::class, [
+                'label' => 'Article',
+            ])
             ->add('date')
-            ->add('theme')
-            ->add('picture')
+            ->add('theme', TextType::class, [
+                'label' => 'Thème'])
+            ->add('picture', TextType::class, [
+                'label' => 'Image'])
         ;
     }
 
