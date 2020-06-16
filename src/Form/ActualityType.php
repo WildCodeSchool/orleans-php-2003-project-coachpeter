eee31281cd04f6cb481952fd20c4a62c3d9f21b9
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-16 14:51:42 +0200

    suppression use inutile

--- src/Form/ActualityType.php
+++ src/Form/ActualityType.php
@@ -9,7 +9,6 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vich\UploaderBundle\Form\Type\VichFileType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ActualityType extends AbstractType
