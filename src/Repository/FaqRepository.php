b05f8ec69aba60d363401c3a907b1d0dcc08feb6
kodev-sudo <k.ollivierdev@gmail.com>
2020-06-17 15:58:36 +0200

    Removing comments in FAQRespository

--- src/Repository/FaqRepository.php
+++ src/Repository/FaqRepository.php
@@ -18,33 +18,4 @@ class FaqRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Faq::class);
     }
-
-    // /**
-    //  * @return Faq[] Returns an array of Faq objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('f.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Faq
-    {
-        return $this->createQueryBuilder('f')
-            ->andWhere('f.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
