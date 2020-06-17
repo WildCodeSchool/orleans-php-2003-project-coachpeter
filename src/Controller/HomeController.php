7255d7e4c31ea42cad83746fee1e53e859c5e4d4
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-17 15:10:22 +0200

    fix limit 3

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends AbstractController
 
         $activities = $this->getDoctrine()
             ->getRepository(Activity::class)
-            ->findBy(['focus' => true]);
+            ->findBy(['focus' => true], [], 3);
 
         $transformations = $this->getDoctrine()
             ->getRepository(Transformation::class)
