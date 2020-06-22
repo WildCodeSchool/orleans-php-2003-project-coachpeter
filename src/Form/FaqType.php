2b0648e6ca249034cbcadec63c5f44b0a3dc0cca
kodev-sudo <k.ollivierdev@gmail.com>
2020-06-22 18:09:06 +0200

    Adding style for New FAQ form

--- src/Form/FaqType.php
+++ src/Form/FaqType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Faq;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,15 @@ class FaqType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('question')
-            ->add('answer')
-        ;
+            ->add('question', TextType::class, [
+                'label' => 'Question',
+                'attr' => ['class' => "col-12"
+            ]])
+            ->add('answer', TextareaType::class, [
+                'label' => 'Réponse',
+                'attr' => ['class' => "col-12"
+            ]]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
