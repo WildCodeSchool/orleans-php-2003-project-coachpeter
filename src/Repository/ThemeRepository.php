8d160618ca6c112d427fcff1cbfdb690c3c784be
Emmanuelle Mendels <61285494+Emmanuelle-Mendels@users.noreply.github.com>
2020-06-23 14:23:57 +0200

    suppression des commentaires

--- src/Repository/ThemeRepository.php
+++ src/Repository/ThemeRepository.php
@@ -18,33 +18,4 @@ class ThemeRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Theme::class);
     }
-
-    // /**
-    //  * @return Theme[] Returns an array of Theme objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('t.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Theme
-    {
-        return $this->createQueryBuilder('t')
-            ->andWhere('t.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
