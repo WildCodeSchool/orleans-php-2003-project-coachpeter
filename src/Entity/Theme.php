29042acd596e4afa988723ba2e53df63ccd6f308
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-23 11:33:36 +0200

    ajout entité thème pour les programmes

---
+++ src/Entity/Theme.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ThemeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ThemeRepository::class)
+ */
+class Theme
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Assert\NotBlank(message="Veuillez indiquer le nom du thème")
+     * @Assert\Length(max=50, maxMessage="Le nom du thème {{ value }} est trop long,
+     * il ne devrait pas dépasser {{ limit}} caractères.")
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
