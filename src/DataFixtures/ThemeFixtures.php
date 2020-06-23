ca3a555956013d9ee77ab3829fe82e88b079b2fb
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-23 11:51:24 +0200

    ajout fixtures

--- src/DataFixtures/ThemeFixtures.php
+++ src/DataFixtures/ThemeFixtures.php
@@ -1,11 +1,10 @@
 <?php
 
-
 namespace App\DataFixtures;
 
 use App\Entity\Theme;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager;
 use Faker;
 
 class ThemeFixtures extends Fixture
@@ -13,12 +12,12 @@ class ThemeFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $faker = Faker\Factory::create('fr_FR');
-        for ($i = 0; $i <= 6; $i++) {
+        for ($i = 1; $i <= 6; $i++) {
             $theme = new Theme();
-            $theme->setName($faker->title);
+            $theme->setName($faker->name);
             $manager->persist($theme);
-            $this->addReference($i, $theme);
         }
+
         $manager->flush();
     }
 }
