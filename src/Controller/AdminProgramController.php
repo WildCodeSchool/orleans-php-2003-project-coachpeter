af1c03435e4ee491ef4ea36da4477d6ca1518889
Philippe NAVEILHAN <travail@naveilhan.com>
2020-06-29 14:00:29 +0200

    francisation de la route

--- src/Controller/AdminProgramController.php
+++ src/Controller/AdminProgramController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("admin/program")
+ * @Route("admin/programme")
  */
 class AdminProgramController extends AbstractController
 {
