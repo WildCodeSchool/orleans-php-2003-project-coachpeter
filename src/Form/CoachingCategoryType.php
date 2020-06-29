6370044bb585ed5ddf54dce01c069797d093b849
malyadev <malyadev248@gmail.com>
2020-06-29 15:15:36 +0200

    correction texttype

--- src/Form/CoachingCategoryType.php
+++ src/Form/CoachingCategoryType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\CoachingCategory;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,7 +13,7 @@ class CoachingCategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('category')
+            ->add('category', TextType::class)
         ;
     }
 
