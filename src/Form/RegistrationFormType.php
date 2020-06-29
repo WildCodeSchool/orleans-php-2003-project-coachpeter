0f74c02b246276627c347bc108ec7ca5545ead19
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-29 14:19:51 +0200

    précision des types de champ

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -3,11 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -19,12 +20,12 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
             ->add('phone', \Symfony\Component\Form\Extension\Core\Type\TextType::class, [
                 'required' => false
             ])
-            ->add('email')
+            ->add('email', EmailType::class)
             ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 // instead of being set onto the object directly,
