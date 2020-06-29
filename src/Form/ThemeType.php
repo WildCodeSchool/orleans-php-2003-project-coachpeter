0a027e756571598e2acc7b19468a9996be47d1f0
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-06-29 14:13:19 +0200

    modification thème

--- src/Form/ThemeType.php
+++ src/Form/ThemeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Theme;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,9 @@ class ThemeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom'
+            ])
         ;
     }
 
