e08cff681eb897a8fc6ba4d6f2f815bb468757bd
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-07-01 08:14:52 +0200

    en attente

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,20 +4,31 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
+    const ROLES = [
+        "Client" => "ROLE_USER",
+        "Membre" => "ROLE_MEMBER",
+        "Administrateur" => "ROLE_ADMIN",];
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
             ->add('email')
-            ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => self::ROLES,
+                'multiple' => false,
+                'expanded' => true,
+                'mapped' => true,
+            ])
             ->add('firstname')
             ->add('lastname')
-            ->add('phone')
-        ;
+            ->add('phone');
     }
 
     public function configureOptions(OptionsResolver $resolver)
