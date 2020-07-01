2032b1d7392a3a354bc825792a3ade9bcfe16104
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-07-01 14:44:33 +0200

    make crud

---
+++ src/Controller/AttendedController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Attended;
+use App\Form\AttendedType;
+use App\Repository\AttendedRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/attended")
+ */
+class AttendedController extends AbstractController
+{
+    /**
+     * @Route("/", name="attended_index", methods={"GET"})
+     */
+    public function index(AttendedRepository $attendedRepository): Response
+    {
+        return $this->render('attended/index.html.twig', [
+            'attendeds' => $attendedRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="attended_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $attended = new Attended();
+        $form = $this->createForm(AttendedType::class, $attended);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($attended);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('attended_index');
+        }
+
+        return $this->render('attended/new.html.twig', [
+            'attended' => $attended,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="attended_show", methods={"GET"})
+     */
+    public function show(Attended $attended): Response
+    {
+        return $this->render('attended/show.html.twig', [
+            'attended' => $attended,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="attended_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Attended $attended): Response
+    {
+        $form = $this->createForm(AttendedType::class, $attended);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('attended_index');
+        }
+
+        return $this->render('attended/edit.html.twig', [
+            'attended' => $attended,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="attended_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Attended $attended): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$attended->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($attended);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('attended_index');
+    }
+}
--- src/Repository/AttendedRepository.php
+++ src/Repository/AttendedRepository.php
@@ -18,33 +18,4 @@ class AttendedRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Attended::class);
     }
-
-    // /**
-    //  * @return Attended[] Returns an array of Attended objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Attended
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
