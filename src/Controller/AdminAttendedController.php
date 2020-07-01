2d8ef4d935800cb12e1c9fbb19d340bd7254574e
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-07-01 15:58:13 +0200

    modif nom route

--- src/Controller/AdminAttendedController.php
+++ src/Controller/AdminAttendedController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("admin/attended")
+ * @Route("admin/adhesion")
  */
 class AdminAttendedController extends AbstractController
 {
