6f878c380f19dd358516d5a38fd940cb2d25cf5e
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-07-01 09:16:01 +0200

    label en français ok

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\User;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -19,16 +22,23 @@ class UserType extends AbstractType
     {
 
         $builder
-            ->add('email')
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom'
+            ])
+            ->add('phone', TextType::class, [
+                'label' => 'Téléphone'
+            ])
+            ->add('email', EmailType::class)
             ->add('roles', ChoiceType::class, [
+                'label' => 'Rôles',
                 'choices' => self::ROLES,
                 'multiple' => true,
                 'expanded' => true,
                 'mapped' => true,
-            ])
-            ->add('firstname')
-            ->add('lastname')
-            ->add('phone');
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
