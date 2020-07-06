0e7e2e647dcc73c6a0dece171536074f7069f0f0
kodev-sudo <k.ollivierdev@gmail.com>
2020-07-06 09:40:36 +0200

    Adding some responsive style and mail data

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\InfoCoach;
+use App\Form\ContactType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -12,8 +17,33 @@ class ContactController extends AbstractController
      * @Route("/contact/", name="app_contact")
      * @return Response
      */
-    public function index(): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
-        return $this->render('contact/contact.html.twig');
+        $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        $coachInfo = $this->getDoctrine()
+            ->getRepository(InfoCoach::class)
+            ->findOneBy([]);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $dataForm = $_POST['contact'];
+            $email = (new Email())
+                ->from($dataForm['email'])
+                ->to('peter.dionisiopro@gmail.com')
+                ->subject('Un nouvelle personnage vous a contacté depuis votre site Internet')
+                ->html('<h3>Une nouvelle personne vous a contacté par le biais du formulaire de contact :</h3>
+                        <p>Prénom : ' . $dataForm['firstname'] . '</p>
+                        <p>Nom : ' . $dataForm['lastname'] . '</p>
+                        <p>Numéro de téléphone : ' . $dataForm['phone'] . '</p>
+                        <p>Email : ' . $dataForm['email'] . '</p>
+                        <p>Message : ' . $dataForm['message'] . '</p>
+                        ');
+
+            $mailer->send($email);
+        }
+        return $this->render('contact/contact.html.twig', [
+            'coachInfo' => $coachInfo,
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Form/ContactType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'attr' => ['class' => "col-12 mb-3", 'placeholder' => "Sandrine"],
+                'label' => 'Prénom',
+                'required'   => true,
+                ])
+            ->add('lastname', TextType::class, [
+                'attr' => ['class' => "col-12 mb-3", 'placeholder' => "Germain" ],
+                'label' => 'Nom',
+                'required'   => true,
+                ])
+            ->add('phone', TextType::class, [
+                'attr' => ['class' => "col-12 mb-3", 'placeholder' => "0687654321"],
+                'label' => 'Numéro de téléphone',
+                'required'   => true,
+                ])
+            ->add('email', EmailType::class, [
+                'attr' => ['class' => "col-12 mb-3", 'placeholder' => "sandrine.germain@gmail.com"],
+                'label' => 'Adresse Email',
+                'required'   => true,
+                ])
+            ->add('message', TextareaType::class, [
+                'attr' => ['class' => "col-12 mb-3", 'placeholder' => "Je suis intéressée par votre programme. Auriez-vous plus d'informations à me communiquer ?"],
+                'label' => 'Message',
+                'required'   => true,
+                ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+        ]);
+    }
+}
