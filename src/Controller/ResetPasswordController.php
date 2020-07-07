7c2e5922e68af7b48da1c99e2ca38f1c71226353
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-07-07 20:10:19 +0200

    messages erreur en français

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -105,14 +105,15 @@ class ResetPasswordController extends AbstractController
 
         $token = $this->getTokenFromSession();
         if (null === $token) {
-            throw $this->createNotFoundException('No reset password token found in the URL or in the session.');
+            throw $this->createNotFoundException('Aucun jeton de réinitialisation de mot de passe 
+            n\'a été trouvé dans l\'URL ou dans la session.');
         }
 
         try {
             $user = $this->resetPasswordHelper->validateTokenAndFetchUser($token);
         } catch (ResetPasswordExceptionInterface $e) {
             $this->addFlash('reset_password_error', sprintf(
-                'There was a problem validating your reset request - %s',
+                'Un problème est survenu lors de la validation de votre demande de réinitialisation - %s',
                 $e->getReason()
             ));
 
@@ -169,7 +170,7 @@ class ResetPasswordController extends AbstractController
             $resetToken = $this->resetPasswordHelper->generateResetToken($user);
         } catch (ResetPasswordExceptionInterface $e) {
             $this->addFlash('reset_password_error', sprintf(
-                'There was a problem handling your password reset request - %s',
+                'Un problème est survenu lors du traitement de votre demande de réinitialisation de mot de passe - %s',
                 $e->getReason()
             ));
 
