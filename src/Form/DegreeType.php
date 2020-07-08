6edd60d1bf74de44ec9f113ea5a83fefe7f6383d
Emmanuelle-Mendels <emmanuelle.mendels@gmail.com>
2020-07-08 18:47:21 +0200

    edition ok

--- src/Form/DegreeType.php
+++ src/Form/DegreeType.php
@@ -3,20 +3,39 @@
 namespace App\Form;
 
 use App\Entity\Degree;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
 
 class DegreeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('organism')
-            ->add('description')
-            ->add('startDate')
-            ->add('endDate')
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+                'attr' => ['class' => "col-12 mb-3"]
+            ])
+            ->add('organism', TextType::class, [
+                'label' => 'Organisme',
+                'attr' => ['class' => "col-12 mb-3"]
+            ])
+            ->add('description', TextType::class, [
+                'label' => 'Description',
+                'attr' => ['class' => "col-12 mb-3"]
+            ])
+            ->add('startDate', NumberType::class, [
+                'label' => 'Date de début',
+                'attr' => ['class' => "col-12 mb-3"]
+            ])
+            ->add('endDate', NumberType::class, [
+                'label' => 'Date de fin',
+                'attr' => ['class' => "col-12 mb-3"]
+            ])
         ;
     }
 
